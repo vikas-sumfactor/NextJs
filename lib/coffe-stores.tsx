bc844de107a8f2bd069db8bd4f356de17bbb84a3
vikas-sumfactor <vikas.sumfactor@gmail.com>
2023-05-09 17:27:52 +0530

    7th section

--- lib/coffe-stores.tsx
+++ lib/coffe-stores.tsx
@@ -1,8 +1,70 @@
+import { createApi } from 'unsplash-js';
+import * as nodeFetch from 'node-fetch'
+
+const unsplashApi = createApi({
+  accessKey: 'UNSPLASH_ACCESS_KEY' ,
+  fetch: nodeFetch.default as unknown as typeof fetch,
+});
+
+// const unsplashApi = createApi({
+
+//   accessKey: 'UNSPLASH_ACCESS_KEY' ,
+  
+// });
+// const getUrlForCoffeeStores = (latLong:any, query:any, limit:any) => {
+//   return `https://api.foursquare.com/v3/places/search?query=${query}&ll=${latLong}&limit=${limit}`;
+// };
+
+// export const fetchCoffeeStores = async () => {
+
+//       const photos    =await unsplashApi.search.getPhotos({
+//     query: "cat",
+//     page: 1,
+//     perPage: 10,
+//     color: "green",
+//     orientation: "portrait",
+//   });
+//   console.log({photos});
+
+//   const options = {
+//     method: "GET",
+//     headers: {
+//       Accept: "application/json",
+//       Authorization: process.env.FOURSQUARE_API_KEY,
+//     },
+//   };
+//   const response = await fetch(
+//     getUrlForCoffeeStores(
+//       "43.653833032607096%2C-79.37896808855945",
+//       "coffee",
+//       6
+//     ),
+//     options
+//   );
+//   const data = await response.json();
+//   return data.results;
+// };
+
+
+
 const getUrlForCoffeeStores = (latLong:any, query:any, limit:any) => {
     return `https://api.foursquare.com/v3/places/search?query=${query}&ll=${latLong}&limit=${limit}`;
   };
+
+
+  const getListOfCoffeeStorePhotos = async () => {
+    const photos = await unsplashApi.search.getPhotos({
+      query: "coffee shop",
+      perPage: 30,
+    });
+    console.log(photos);
+   const unsplashResults = photos.response.results;
+   return unsplashResults.map((result) => result.urls["small"]);
+  };
   
   export const fetchCoffeeStores = async () => {
+    const photos = await getListOfCoffeeStorePhotos();
+
     const options = {
       method: "GET",
       headers: {
@@ -20,5 +82,11 @@ const getUrlForCoffeeStores = (latLong:any, query:any, limit:any) => {
       options
     );
     const data = await response.json();
-    return data.results;
+    return data.results.map((result:any, idx:any) => {
+      return {
+        ...result,
+        imgUrl: photos[idx],
+      };
+    });
   };
+  export default unsplashApi;
